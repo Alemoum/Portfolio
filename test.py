9123baf5b058ab77801c744f05466f797fcc9aca
Alemoum <marcosvmm3110@gmail.com>
2025-06-02 15:07:56 -0300

    Some changes

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = "lalala.md"
-
-list_splited_text = list(os.path.splitext(path))
-list_splited_text[1] = ".html"
-new_path = list_splited_text[0] + list_splited_text[1]
-
-print(new_path)
